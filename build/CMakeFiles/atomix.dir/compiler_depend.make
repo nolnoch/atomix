d43e75f0ae16e1e0baae2009c41f2398b4b7dcb5
Wade Burch <braernoch@gmail.com>
2023-10-13 16:28:31 -0600

    Clean repo

--- build/CMakeFiles/atomix.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for atomix.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/atomix_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/atomix_autogen"
-  "atomix_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/atomix_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

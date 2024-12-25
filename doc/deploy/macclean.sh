131e5951586e2deaef121f9b34fb6f44ea6d3186
Wade Burch <braernoch@gmail.com>
2024-12-25 09:01:21 -0700

    What luck! VSCode CMake Tools ext just updated to support v9 macro expansion. Mac build refactored to use script. All builds stable. Gitignoring UserPresets.

---
+++ doc/deploy/macclean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -rf deploy.sh package install/*
+echo Done cleaning.
+
--- doc/deploy/macdeploy.sh
+++ doc/deploy/macdeploy.sh
@@ -1,20 +1,41 @@
 #!/bin/sh
 
-LIBDIR="install/atomix.app/Contents/Frameworks"
-RESDIR="install/atomix.app/Contents/Resources"
-SRCDIR=".."
+SRCDIR="/Users/braer/dev/atomix"
+BLDDIR="/Users/braer/dev/atomix/build"
+INSTDIR="/Users/braer/dev/atomix/build/install"
+VKDIR="/Users/braer/VulkanSDK/1.3.296.0/macOS"
+QTDIR="/usr/local/Qt-6.8.0"
+LIBDIR="atomix.app/Contents/Frameworks"
+RESDIR="atomix.app/Contents/Resources"
 
-mkdir -p $RESDIR/configs
-mkdir -p $RESDIR/shaders
-mkdir -p $RESDIR/res
+mkdir -p $INSTDIR 
+cp -r src/Release/atomix.app $INSTDIR/
+
+cd $INSTDIR
+
+mkdir -p $RESDIR
 mkdir -p $LIBDIR
 
-cp -r $SRCDIR/configs/* $RESDIR/configs/
-cp -r $SRCDIR/shaders/* $RESDIR/shaders/
-cp -r $SRCDIR/res/* $RESDIR/res/
+cp -r $SRCDIR/configs $RESDIR/
+cp -r $SRCDIR/shaders $RESDIR/
+cp -r $SRCDIR/res $RESDIR/
 cp $SRCDIR/res/icons/atomix.icns $RESDIR/
-cp vcpkg_installed/$VCPKG_TARGET_TRIPLET/lib/libvulkan.1.3.296.dylib $LIBDIR/
-cp vcpkg_installed/$VCPKG_TARGET_TRIPLET/lib/libMoltenVK.dylib $LIBDIR/
+cp $VKDIR/lib/libvulkan.1.3.296.dylib $LIBDIR/
+cp $VKDIR/lib/libMoltenVK.dylib $LIBDIR/
 
-ln -s $LIBDIR/libvulkan.1.3.296.dylib $LIBDIR/libvulkan.1.dylib
-ln -s $LIBDIR/libvulkan.1.3.296.dylib $LIBDIR/libvulkan.dylib
+cd $LIBDIR
+
+ln -s libvulkan.1.3.296.dylib libvulkan.1.dylib
+ln -s libvulkan.1.dylib libvulkan.dylib
+
+cd $INSTDIR
+
+$QTDIR/bin/macdeployqt atomix.app -libpath=$QTDIR/lib -codesign="Apple Development: braernoch@gmail.com (PC3TVMS8YJ)"
+
+# codesign --force --deep --sign "Apple Development: braernoch@gmail.com (PC3TVMS8YJ)" atomix.app
+codesign --verify --verbose atomix.app
+
+# cd $BLDDIR
+
+# mkdir package
+# cp install/atomix.app package/

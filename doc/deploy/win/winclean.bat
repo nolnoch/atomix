c1b4ce3e8a27c9789d302f7340ed4af650626e5e
Wade Burch <braernoch@gmail.com>
2024-12-26 15:48:57 -0700

    [Checkpoint] Windows build system refactor complete. All Windows builds stable.

--- doc/deploy/win/winclean.bat
+++ doc/deploy/win/winclean.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 
-rd /s /q package\atomix
+rd /s /q deploy
 
 del /s /f /q install\*.*
 for /f %%f in ('dir /ad /b install') do rd /s /q install\%%f
 
 del deploy.bat
+
+echo "Done cleaning."
--- doc/deploy/win/windeploy.bat
+++ doc/deploy/win/windeploy.bat
@@ -1,9 +1,16 @@
 @ECHO OFF
 
+set CFG=%1
+set QTDEPLOY="C:\Qt\6.8.1\msvc2022_64\bin\windeployqt.exe"
+set QTBINARY="C:\Qt\Tools\QtInstallerFramework\4.8\bin\binarycreator.exe"
+set QTBINBASE="C:\Qt\Tools\QtInstallerFramework\4.8\bin\installerbase.exe"
 set INDIR="install"
-set DATADIR="package\atomix\packages\com.nolnoch.atomix\data"
-set METADIR="package\atomix\packages\com.nolnoch.atomix\meta"
-set CFGDIR="package\atomix\config"
+set DATADIR="deploy\packages\com.nolnoch.atomix\data"
+set METADIR="deploy\packages\com.nolnoch.atomix\meta"
+set CFGDIR="deploy\config"
+
+@REM echo "%QTCMD% --dir=%INDIR% src\%CFG%\atomix.exe"
+%QTDEPLOY% --dir=%INDIR% src\%CFG%\atomix.exe
 
 md %CFGDIR%
 md %METADIR%
@@ -11,10 +18,18 @@ md %DATADIR%\configs
 md %DATADIR%\shaders
 md %DATADIR%\res
 
-xcopy /s %INDIR%\* %DATADIR%\
-xcopy /s ..\configs %DATADIR%\configs
-xcopy /s ..\shaders %DATADIR%\shaders
-xcopy /s ..\res %DATADIR%\res
+xcopy /s /e %INDIR%\* %DATADIR%\
+xcopy /s /e ..\configs %DATADIR%\configs
+xcopy /s /e ..\shaders %DATADIR%\shaders
+xcopy /s /e ..\res %DATADIR%\res
 
 xcopy ..\doc\deploy\win\meta\* %METADIR%\
 xcopy ..\doc\deploy\win\config.xml %CFGDIR%\
+
+echo "Done installing."
+echo "Begin packaging."
+cd deploy
+
+%QTBINARY% --offline-only -t %QTBINBASE% -c config\config.xml -p packages atomix-installer-win64.exe
+
+echo "Done packaging."
